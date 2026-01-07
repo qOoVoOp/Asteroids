d59f87ac9a198330d8685c68455fb395fe8a67f1
qOoVoOp <dfprivateacc@gmail.com>
2026-01-08 00:37:46 +0100

    Added clock/Tick

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock =pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     
     while True:
         log_state()
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
-        pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        print(dt)
+
+    screen.fill("black")
+    pygame.display.flip()
+
+
 
 if __name__ == "__main__":
     main()
