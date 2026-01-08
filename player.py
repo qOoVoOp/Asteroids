ca164b81735a5f76b13a42d5ff90bfb7694aeaad
qOoVoOp <dfprivateacc@gmail.com>
2026-01-08 01:53:11 +0100

    004: Added fw/bw movement

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 from circleshape import CircleShape
 import pygame
 
@@ -16,10 +16,24 @@ class Player(CircleShape):
       c = self.position - forward * self.radius + right
       return [a, b, c]
   
+
+  def move(self,dt):
+    unit_vector = pygame.Vector2(0, 1)
+    rotated_vector = unit_vector.rotate(self.rotation)
+    rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+    self.position += rotated_with_speed_vector
+
+
   def update(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotation += PLAYER_TURN_SPEED * dt
-        if keys[pygame.K_d]:
             self.rotation -= PLAYER_TURN_SPEED * dt
+        if keys[pygame.K_d]:
+            self.rotation += PLAYER_TURN_SPEED * dt
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move((-dt))
+
+    
